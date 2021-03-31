0b8debf3516ec86726852893252b6c5f8e3741f3
chandni0202 <mishra.chandni14@gmail.com>
2021-03-31 16:39:06 +0530

    deployment

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../Presentations/CardList';
 import SearchBox from '../Presentations/SearchBox';
 import Scroll from '../Presentations/Scroll';
+import Error from '../Presentations/Error';
 
 class App extends Component {
   constructor() {
@@ -34,7 +35,9 @@ class App extends Component {
           <h1 className='f1'>RoboFamily</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
+          <Error>
             <CardList robots={filteredRobots} />
+          </Error>
         </Scroll>
         </div>
       );
