29763e84c722ad3538f22394b3a716a63b6cfc42
chandni0202 <mishra.chandni14@gmail.com>
2021-03-31 17:56:28 +0530

    Updates

--- src/Presentations/Scroll.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-const Scroll = (props) => {
-  return <div style={{overflow: 'scroll', border: '3px #000000 solid', height: '1000px'}}>{props.children}</div>
-}
-
-export default Scroll;
